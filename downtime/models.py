f2e8e53750af1bead4bd9725c6af3ff0acc4f6ce
Erik <xxxsmoizxxx@gmail.com>
2014-11-30 20:55:43 +0100

    Expanded the downtime model.

--- downtime/models.py
+++ downtime/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# Characters models
 class Discipline(models.Model):
     name = models.CharField(max_length=200)
 
@@ -20,17 +21,28 @@ class Ability(models.Model):
             return self.name
 
 class Boon(models.Model):
-    type = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    value = models.IntegerField()
 
     def __str__(self):
-            return self.type
+            return self.name
 
 class Character(models.Model):
     name = models.CharField(max_length=200)
     user = models.ForeignKey(User)
-    disciplines = models.ManyToManyField(Discipline)
-    abilities = models.ManyToManyField(Ability)
-    titles = models.ManyToManyField(Title)
+    disciplines = models.ManyToManyField(Discipline, blank=True)
+    abilities = models.ManyToManyField(Ability, blank=True)
+    titles = models.ManyToManyField(Title, blank=True)
 
     def __str__(self):
             return self.name
+
+class Debt(models.Model):
+    count = models.IntegerField()
+    size = models.ForeignKey(Boon)
+    creditor = models.ForeignKey(Character, related_name="credits")
+    debtor = models.ForeignKey(Character, related_name="debts")
+    description = models.TextField()
+
+    def __str__(self):
+            return "%s owes %s: %d %s" % (self.debtor, self.creditor, self.count, self.size)
