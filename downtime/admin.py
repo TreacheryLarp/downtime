bf8d3cee64f59246a2355a61a9d76a91143b6b78
Erik <xxxsmoizxxx@gmail.com>
2014-12-03 14:54:50 +0100

    Updated model with actions and feeding

--- downtime/admin.py
+++ downtime/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from downtime.models import *
+from reversion.helpers import patch_admin
 
+# Admin interface
 admin.site.register(Character)
 admin.site.register(Discipline)
 admin.site.register(Title)
 admin.site.register(Ability)
 admin.site.register(Boon)
 admin.site.register(Debt)
+admin.site.register(Age)
+admin.site.register(ActionType)
+admin.site.register(Action)
+admin.site.register(Domain)
+admin.site.register(Session)
+
+
+# reversion
+patch_admin(Character)
--- downtime/models.py
+++ downtime/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import reversion
 
 # Characters models
 class Discipline(models.Model):
@@ -20,12 +21,18 @@ class Ability(models.Model):
     def __str__(self):
             return self.name
 
+class Age(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+            return self.name
+
 class Boon(models.Model):
     name = models.CharField(max_length=200)
     value = models.IntegerField()
 
     def __str__(self):
-            return self.name
+        return self.name
 
 class Character(models.Model):
     name = models.CharField(max_length=200)
@@ -33,9 +40,10 @@ class Character(models.Model):
     disciplines = models.ManyToManyField(Discipline, blank=True)
     abilities = models.ManyToManyField(Ability, blank=True)
     titles = models.ManyToManyField(Title, blank=True)
+    age = models.ForeignKey(Age)
 
     def __str__(self):
-            return self.name
+        return self.name
 
 class Debt(models.Model):
     count = models.IntegerField()
@@ -45,4 +53,35 @@ class Debt(models.Model):
     description = models.TextField()
 
     def __str__(self):
-            return "%s owes %s: %d %s" % (self.debtor, self.creditor, self.count, self.size)
+        return "%s owes %s: %d %s" % (self.debtor, self.creditor, self.count, self.size)
+
+# Actions
+class Domain(models.Model):
+    name = models.CharField(max_length=200)
+    feeding_capacity = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class ActionType(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Session(models.Model):
+    name = models.CharField(max_length=200)
+    is_open = models.BooleanField(default=True)
+    feeding_domains = models.ManyToManyField(Domain, blank=True)                # finns det risk
+    active_character = models.ManyToManyField(Character, blank=True)
+
+    def __str__(self):
+            return self.name
+
+class Action(models.Model):
+    action_type = models.ForeignKey(ActionType)
+    character = models.ForeignKey(Character)
+    session = models.ForeignKey(Session, related_name="actions")
+
+    def __str__(self):
+        return "[s] %s" % (self.character, self.action_type)
