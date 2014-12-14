42b410fa2e0728296b52259efff78630778a3eea
Erik <xxxsmoizxxx@gmail.com>
2014-12-14 01:52:30 +0100

    Refactored wizard to a function instead of in urls.py

--- downtime/urls.py
+++ downtime/urls.py
@@ -1,20 +1,10 @@
 from django.conf.urls import patterns, url
-from django.contrib.auth.decorators import login_required, permission_required
-from django.forms.models import modelformset_factory
 
 from downtime import views
-from downtime import forms
-from downtime import models
 
 urlpatterns = patterns('',
     url(r'^$', views.profile, name='profile'),
     url(r'^profile/$', views.profile, name='profile'),
-    url(r'^s/(?P<pk>\d+)/$', views.session, name='session'),
-
-    url(r'^s/submit/(?P<pk>\d+)$',
-        views.SubmitWizard.as_view(
-        [forms.DisciplineActivationForm, forms.FeedingForm,
-        modelformset_factory(models.Action, formset=forms.ActionFormSet,
-        fields=('action_type', 'description'))]),
-        name='wizard'),
+    url(r'^s/(?P<session>\d+)/$', views.session, name='session'),
+    url(r'^s/submit/(?P<session>\d+)$', views.wizard, name='wizard'),
 )
--- downtime/views.py
+++ downtime/views.py
@@ -6,8 +6,10 @@ from django.contrib.auth.decorators import login_required
 from django.db import models
 from django.contrib.formtools.wizard.views import SessionWizardView
 from django.utils.decorators import method_decorator
+from django.forms.models import modelformset_factory
 
-from downtime.models import Session
+from downtime import forms
+from downtime.models import Session, Action
 
 @login_required
 def profile(request):
@@ -15,29 +17,31 @@ def profile(request):
                                                      'session_list': Session.objects.all()})
 
 @login_required
-def session(request, pk):
-    session = get_object_or_404(Session, pk=pk)
+def session(request, session):
+    session = get_object_or_404(Session, pk=session)
     character = request.user.character
     data =  {'session': session,
-             'character': character,
-             'has_submitted': session.has_submitted(character)}
+             'character': character}
     return render(request, 'downtime/session.html', data)
+
+@login_required
+def wizard(request, session):
+    return SubmitWizard.as_view([
+        forms.DisciplineActivationForm,
+        forms.FeedingForm,
+        modelformset_factory(Action, formset=forms.ActionFormSet, fields=('action_type', 'description'))
+        ])(request, session=session)
+
 
 class SubmitWizard(SessionWizardView):
     template_name = 'downtime/submit_wizard.html'
-
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        session =  get_object_or_404(Session, pk=kwargs['pk'])
-        character = self.request.user.character
-        return super(SessionWizardView, self).dispatch(*args, **kwargs)
 
     def get_form_kwargs(self, step):
         return {'user': self.request.user}
 
     def done(self, form_list, **kwargs):
-        session =  get_object_or_404(Session, pk=kwargs['pk'])
+        session =  get_object_or_404(Session, pk=kwargs['session'])
         character = self.request.user.character
         for f in form_list:
             f.fill_save(session, character)
-        return HttpResponseRedirect('/s/%s' % kwargs['pk'])
+        return HttpResponseRedirect('/s/%s' % kwargs['session'])
