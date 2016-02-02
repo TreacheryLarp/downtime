b0f1d6d090d6539ed9340ff1b58f5b5a03d31474
Erik Gärtner <xxxsmoizxxx@gmail.com>
2016-02-02 13:45:03 +0100

    Implemented email notifications for GM comments #20

--- gamemaster/models.py
+++ gamemaster/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django_comments.signals import comment_was_posted
+from django.core.mail import send_mail
 
-# Create your models here.
+
+# Signals
+def notify_user_about_comment(sender, **kwargs):
+    try:
+        comment = kwargs['comment']
+
+        # Only send on GM comment
+        if not comment.user.is_staff:
+            return
+
+        target_object = comment.content_object
+        receipient_user = target_object.character.user
+        to_address = receipient_user.email
+        html = """
+        <html>
+        <body>
+            <h2>Treachery Downtime Notification</h2>
+            <p>
+                Hi %s!
+                <br><br>
+                You have recieved a new comment on one of your downtime actions. Please login and reply if needed.
+                <br><br>
+                <b>%s</b> commented on your action <i>%s</i>:
+                <br><br>
+                <div style='padding-left:10px'>
+                %s
+                </div>
+            </p>
+        </body>
+        </html>
+        """ % (receipient_user.username, comment.name, target_object, comment.comment)
+
+        send_mail('[Treachery Downtime]: New comment', '', '',
+        [to_address], fail_silently=True, html_message=html)
+    except:
+        print('Error while sending mail notification: %s' % kwargs)
+
+comment_was_posted.connect(notify_user_about_comment)
