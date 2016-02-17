ccd31a31910c15eba26a476cb2f80f0bfd04b192
Erik Gärtner <xxxsmoizxxx@gmail.com>
2016-02-17 15:01:48 +0100

    Added testing with fixtures

---
+++ tests/players/tests_login.py
@@ -0,0 +1,16 @@
+from django.test import TestCase, Client
+
+from players.models import *
+
+class LoginTests(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='erik', email='erik@mail', password='erik')
+        self.client = Client()
+
+    def testLogin(self):
+        response = self.client.get('/login/')
+        self.assertContains(response, 'login')
+
+        response = self.client.post('/login/', {'username': self.user.username, 'password': self.user.password})
+        self.assertContains(response, 'login')
